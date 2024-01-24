f904ac40534c0da22c0a704bb74ca6d0a82bbe13
DiegoPF77 <diegopitarch00@gmail.com>
2024-01-24 12:35:21 +0100

    commit prueba

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import geopandas as gpd
 from shapely.geometry import Point
 import xml.etree.ElementTree as ET
+import kml2geojson
+import json
+import os
 
 def parse_kml_coordinates(kml_file):
     tree = ET.parse(kml_file)
@@ -19,29 +22,46 @@ def parse_kml_coordinates(kml_file):
 
     return coordinates
 
+def kml_to_geojson(kml_file):
+    geojson_data = kml2geojson.convert(kml_file)
+    return geojson_data
 
 def kml_to_geodataframe(kml_file):
     coordinates = parse_kml_coordinates(kml_file)
     gdf = gpd.GeoDataFrame(geometry=coordinates)
+    
+    # Crear la columna 'lat' a partir de la segunda componente en las coordenadas
+    gdf['lat'] = gdf['geometry'].apply(lambda point: point.y)
+    
+    # Eliminar la columna 'geometry' ya que 'lat' contiene la información necesaria
+    gdf = gdf.drop('geometry', axis=1)
+    
     return gdf
 
 def main():
     st.title("Visualizador de Coordenadas KML")
 
-    # Archivo KML
-    kml_file = st.file_uploader("Cargar archivo KML", type=["kml"])
+    # Especificar la ruta del archivo KML directamente
+    kml_file_path = "Indicaciones de C del Clariano, 11 a C dels Sants Just i Pastor, 153.kml"
 
-    if kml_file is not None:
-        st.subheader("Mapa de Coordenadas")
+    # Transformar a GeoJSON
+    geojson_data = kml_to_geojson(kml_file_path)
+    
+    # Crear GeoDataFrame
+    gdf = kml_to_geodataframe(kml_file_path)
 
-        # Crear GeoDataFrame
-        gdf = kml_to_geodataframe(kml_file)
+    # Crear un directorio temporal para almacenar el archivo GeoJSON
+    temp_dir = os.path.join(os.path.dirname(__file__), "temp")  # Puedes cambiar "temp" a cualquier nombre de directorio que desees
+    os.makedirs(temp_dir, exist_ok=True)
 
-        gdf['latitude'] = gdf['geometry'].y
-        gdf['longitude'] = gdf['geometry'].x
+    # Guardar el GeoDataFrame en formato GeoJSON temporal
+    temp_geojson_path = os.path.join(temp_dir, "temp.geojson")
+    gdf.to_file(temp_geojson_path, driver="GeoJSON")
 
-        # Mostrar el mapa especificando las columnas de latitud y longitud
-        st.map(data=gdf, lat_column='latitude', lon_column='longitude')
+    # Mostrar el mapa especificando las columnas de latitud y longitud
+    st.map(data=gdf, lat_column='lat', lon_column='longitude')
 
 if __name__ == "__main__":
     main()
+
+
