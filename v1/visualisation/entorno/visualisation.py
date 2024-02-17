97385c1c82623926c229db2ebf694d544ae71b13
Paugb124 <78257330+Paugb124@users.noreply.github.com>
2024-02-17 21:09:02 +0100

    avance visualización

--- v1/visualisation/entorno/visualisation.py
+++ v1/visualisation/entorno/visualisation.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pandas as pd
 import json
-import stramlit_folium
+import streamlit_folium
 from streamlit_folium import folium_static
 import folium 
+from folium.plugins import FloatImage
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import re
 
+
+@st.cache_data(ttl=600)
 # Function to fetch data from GeoJSON file
 def fetch_data_from_json():
     # Read GeoJSON data into a GeoDataFrame
@@ -42,3 +48,70 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# Create API client.
+client = bigquery.Client()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
+rows = run_query('''SELECT driver_id, pickup_location, dropoff_location 
+                    FROM `involuted-river-411314.dp2.trips`
+                    WHERE driver_id = "5254KII"
+                    LIMIT 1''')
+
+st.write(rows)
+
+
+
+points = []
+point_strs = []
+pickup = rows[0]['pickup_location']
+dropoff = rows[0]['dropoff_location']
+point_strs.append(pickup)
+point_strs.append(dropoff)
+converted_points = []
+
+for point_str in point_strs:
+    # Use regular expression to extract numeric values
+    matches = re.findall(r"[-+]?\d*\.\d+|\d+", point_str)
+    
+    # Extract latitude and longitude values
+    latitude = matches[0]
+    longitude = matches[1]
+    
+    # Append formatted point to the list
+    converted_points.append(f"{latitude}, {longitude}")
+
+# Splitting each string into latitude and longitude components
+split_data = [item.split(", ") for item in converted_points]
+
+# Creating a DataFrame
+converted_points = pd.DataFrame(split_data, columns=['lat', 'lon'])
+
+st.write(converted_points)
+
+converted_points['lat'] = converted_points['lat'].astype(float)
+converted_points['lon'] = converted_points['lon'].astype(float)
+
+# Create a map centered at a specific location
+m = folium.Map(location=[converted_points['lat'].mean(), converted_points['lon'].mean()], zoom_start=14)
+
+# Add markers with custom images
+for _, row in converted_points.iterrows():
+    folium.Marker([row['lat'], row['lon']], 
+                  icon=folium.CustomIcon('./images/Black.webp', icon_size=(100, 100))).add_to(m)
+
+# Display the map
+folium_static(m)
+
